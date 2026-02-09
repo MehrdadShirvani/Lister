c4f3d0a1b8a654e4bb99903052ce71f23cac2493
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2026-02-09 23:14:10 +0330

    feat: add init file for models

---
+++ backend/models/__init__.py
@@ -0,0 +1,41 @@
+# Account models
+from .account_models import AccountStatus, Account
+
+# List models
+from .list_models import List
+
+# Task models
+from .task_models import Task, TaskUrl, Suggestion
+
+# Tag models
+from .tag_models import Tag
+
+# Notification models
+from .notification_models import Notification
+
+# Note models
+from .note_models import Note
+
+# Plan models
+from .plan_models import Plan
+
+# TimeBlock models
+from .timeblock_models import TimeBlock
+
+from .task_models import TaskUrl, list_tasks, task_tags
+from .list_models import list_tags
+from .tag_models import timeblock_tags
+
+__all__ = [
+    'AccountStatus',
+    'Account',
+    'List',
+    'Task',
+    'TaskUrl',
+    'Suggestion',
+    'Tag',
+    'Notification',
+    'Note',
+    'Plan',
+    'TimeBlock',
+]
