0f00f07ec01f8056539a71d32be2593d095d7142
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2026-02-13 13:43:56 +0330

    feat: integrate timeblocks with main

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ import uvicorn
 from core.config import settings
 from core.database import engine, Base
 # from routers import accounts, lists, tasks, tags, notifications, notes, plans, suggestions
-from routers import auth, tags
+from routers import auth, tags, timeblocks
 from models.account_models import AccountStatus, Account
 from models.list_models import List
 from models.task_models import Task, TaskUrl, Suggestion
@@ -74,6 +74,7 @@ app.include_router(auth.router, prefix=settings.API_V1_STR)
 # app.include_router(lists.router, prefix=settings.API_V1_STR)
 # app.include_router(tasks.router, prefix=settings.API_V1_STR)
 app.include_router(tags.router, prefix=settings.API_V1_STR)
+app.include_router(timeblocks.router, prefix=settings.API_V1_STR)
 # app.include_router(notifications.router, prefix=settings.API_V1_STR)
 # app.include_router(notes.router, prefix=settings.API_V1_STR)
 # app.include_router(plans.router, prefix=settings.API_V1_STR)
