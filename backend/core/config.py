2eff842c300bc31fa2b58b0fa260ec4064a600b3
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2026-02-15 00:38:01 +0330

    feat: add backend cors as env

--- backend/core/config.py
+++ backend/core/config.py
@@ -18,8 +18,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     # CORS
-    BACKEND_CORS_ORIGINS: list = ["http://localhost:3000", "http://localhost:5173","http://localhost:8000"]
-    
+    BACKEND_CORS_ORIGINS = os.getenv("BACKEND_CORS_ORIGINS", "").split(",")
     # Database Settings
     DB_USER: str = os.getenv("DB_USER", "postgres")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
